3f7ea171d555d768fc1ee324d7be57c7573a207a
Bruno Mariz <bmarizot@gmail.com>
2022-06-28 16:15:27 -0300

    feat: add real data

--- src/components/Service.tsx
+++ src/components/Service.tsx
@@ -1,5 +1,5 @@
-import React from "react";
-import { PetshopService } from "../@types/petshopService";
+import React from 'react';
+import { PetshopService } from '../@types/petshopService';
 
 type Props = {
   petshopService: PetshopService;
@@ -7,32 +7,39 @@ type Props = {
 };
 
 function Service({ petshopService, clearBg }: Props) {
-  const realLocale = Intl.NumberFormat("pt-BR", {
-    style: "currency",
-    currency: "brl",
+  const capitalizeFirstLetter = (sentence: string) => {
+    const firstLetterCapitalized =
+      sentence.charAt(0).toUpperCase() + sentence.slice(1);
+    return firstLetterCapitalized;
+  };
+  const realLocale = Intl.NumberFormat('pt-BR', {
+    style: 'currency',
+    currency: 'brl',
   });
   return (
     <li
       className={
         `px-5 py-2 w-full  bg-metropoleGreen ` +
-        `${clearBg ? "bg-opacity-20" : "bg-opacity-75"}`
+        `${clearBg ? 'bg-opacity-20' : 'bg-opacity-75'}`
       }
     >
-      <div className="text-3xl my-2">{petshopService.title}</div>
-      <div className="my-2 italic">{petshopService.description}</div>
-      <div className="text-2xl flex justify-between items-center my-2">
+      <div className='text-3xl my-2 capitalize'>{petshopService.title}</div>
+      <div className='my-2 italic opacity-75'>
+        {capitalizeFirstLetter(petshopService.description)}
+      </div>
+      <div className='text-2xl flex justify-between items-center my-2'>
         <span>
-          {typeof petshopService.price == "number"
+          {typeof petshopService.price == 'number'
             ? realLocale.format(petshopService.price)
             : `Valor: ${petshopService.price}`}
         </span>
         <span>
           <button>
             <a
-              target={"_blank"}
+              target={'_blank'}
               href={`https://wa.me/${process.env.REACT_APP_TEL_NUMBER}`}
               className={`${
-                clearBg ? "bg-metropoleGreen" : "bg-metropoleTeal"
+                clearBg ? 'bg-metropoleGreen' : 'bg-metropoleTeal'
               } p-1 px-8 rounded-md text-lg block font-bold`}
             >
               Contato
--- src/data/services.ts
+++ src/data/services.ts
@@ -1,29 +1,145 @@
-import { PetshopService } from "../@types/petshopService";
+import { PetshopService } from '../@types/petshopService';
 
 export const services: PetshopService[] = [
   {
-    title: "Banho P",
-    description:
-      "Banho para cachorros pequenos. Explicabo maiores aspernatur alias magni nostrum blanditiis enim?",
-    price: 60,
+    title: 'banho: PP',
+    description: `banho secagem e escovaçao da pelagem. incluso escovação de dentes, limpeza de orelhas, corte de unhas, perfume, enfeite
+
+animais até 3 kg e das seguintes raças:
+pincher/chihuahua PP, yorkshire PP`,
+    price: 55.0,
+  },
+
+  {
+    title: 'banho: p',
+    description: `banho secagem e escovaçao da pelagem. incluso escovação de dentes, limpeza de orelhas, corte de unhas, perfume, enfeite
+
+animais até 10 kg e das seguintes raças:
+bulldog francês, dachshund, fox, jack russel, spitz pequeno, maltês, pincher, poodle P, pug, galgo italiano, srd P, schnauzer, shih tzu pelo curto, lhasa pelo curto, pequinês, yorkshire p`,
+    price: 60.0,
+  },
+
+  {
+    title: 'banho: m',
+    description: `banho secagem e escovaçao da pelagem. incluso escovação de dentes, limpeza de orelhas, corte de unhas, perfume, enfeite
+
+animais até 20 kg e das seguintes raças:
+beagle, bichon frisé, cocker, cavalier, coton, welsh corgi, srdm, spitz medio, spitz japones, vzsla, whippet, pastor de shetland, west higland terrier, animais tamanho P de pelagem longa`,
+    price: 70.0,
+  },
+
+  {
+    title: 'banho: g',
+    description: `banho secagem e escovaçao da pelagem. incluso escovação de dentes, limpeza de orelhas, corte de unhas, perfume, enfeite
+
+border collie, buldog ingles, bull terrier, boxer, basset hound, dalmata, pit bull, srd g, weimaraner`,
+    price: 80.0,
+  },
+
+  {
+    title: 'banho: GG',
+    description: `banho secagem e escovaçao da pelagem. incluso escovação de dentes, limpeza de orelhas, corte de unhas, perfume, enfeite
+
+akita, golden, pastor alemão, samoieda, chow chow, husky siberiano, labrador, rhodesian, rottweiler`,
+    price: 110.0,
+  },
+
+  {
+    title: 'banho: xGG',
+    description: `banho secagem e escovaçao da pelagem. incluso escovação de dentes, limpeza de orelhas, corte de unhas, perfume, enfeite
+
+bernese, dogue alemão, kuvasz`,
+    price: 130.0,
+  },
+
+  {
+    title: 'banho: gatos',
+    description: `banho secagem e escovaçao da pelagem. incluso limpeza de orelhas, corte de unhas, perfume, enfeite
+
+srd, siames, persa, sagrado da birmania, `,
+    price: 90.0,
+  },
+
+  {
+    title: 'tosa higienica p',
+    description: `corte dos pelos das regiões: embaixo das patinhas , ao redor do  nus e barriga. realizada nos animais de tamanho P e m
+`,
+    price: 20.0,
   },
   {
-    title: "Banho M",
-    description:
-      "Banho para cachorros medios.  Sunt dicta excepturi aperiam eligendi neque quaerat fuga libero temporibus, eum, illo, velit consectetur. ",
-    price: 69.99,
+    title: 'tosa higienica g/GG',
+    description: `corte dos pelos das regiões: embaixo das patinhas , ao redor do  nus e barriga. realizada nos animais de tamanho g e GG
+`,
+    price: 40.0,
   },
-  { title: "Banho G", description: "Banho para cachorros grandes", price: 80 },
+
   {
-    title: "Tosa",
-    description:
-      "Corte dos pelos dos cachorros. Lorem ipsum dolor sit amet consectetur adipisicing elit.",
-    price: "A combinar",
+    title: 'meia tosa p',
+    description: `corte dos pelos das regiões: embaixo das patinhas , ao redor do  nus e barriga e corte dos pelos da região da cabeça realizada nos animais de tamanho p
+`,
+    price: 30.0,
   },
+
   {
-    title: "Tosa Higiênica",
-    description:
-      "Corte do cabelo das patas. Lorem ipsum dolor sit amet consectetur adipisicing elit.",
-    price: 49.5,
+    title: 'tosa ',
+    description: `corte dos pelos de todo o corpo, deixando o pelo no modelo e altura solicitados
+`,
+    price: 90.0,
   },
+
+  {
+    title: 'mascara de hidratação p/M - sense',
+    description: `complexo de lipoproteínas que hidrata e recupera a pelagem, contém extrato de aloe vera, que ajuda a hidratar, tonificar e condicionar os fios.`,
+    price: 40.0,
+  },
+
+  {
+    title: 'mascara de nutrição p/M -  flash',
+    description: `ação termo protetora: protege dos danos causados pelo calor do secador e chapinha;
+• contém óleo de argan, que hidrata, aumenta a elasticidade e diminui o frizz;
+• indicado para cães e gatos de todas as raças`,
+    price: 20.0,
+  },
+
+  {
+    title: 'mascara de reconstrução p/M -  liso',
+    description: `máscara de hidratação multifuncional;
+• promove a reconstrução e recuperação da pelagem ;
+• proporciona brilho, melhor acabamento e maciez à pelagem, deixando-a mais lisa. • contém óleos de crambe e moringa, que têm ação condicionante e melhora a penteabilidade; além de queratina, creatina e d-pantenol, que têm ação reestruturadora, restauradora, hidratante e umectante;
+• indicado para cães de pelagem danificada longa e lisa.`,
+    price: 20.0,
+  },
+
+  {
+    title: 'mascara de reconstrução p/M -  volume',
+    description: `máscara de hidratação multifuncional;
+• promove a reconstrução e recuperação da pelagem;
+• confere brilho e maciez;
+• contém proteína do trigo e d-pantenol, que confere maior volume a pelagem, ação hidratante e umectante; além de queratina e creatina, que tem ação reestruturadora, restauradora e hidratante;
+• indicado para cães e gatos de pelagem danificada e volumosa;`,
+    price: 20.0,
+  },
+
+  {
+    title: 'desembolo ',
+    description: `escovaçao com uso de fluido desembaraçante que remove os nós que se formam nos animais de pelagem média ou longa
+`,
+    price: 'cobrado de acordo com avaliação individual',
+  },
+
+  {
+    title: 'carding p/M',
+    description: `consiste em uma técnica de escovação indolor na qual é realizada a retirada de subpelos (pelos mortos). com ela, aqueles pelinhos mais finos e não vistos, que sempre caem pela casa ou formam nós, são eliminados de forma rápida e eficiente.
+`,
+    price: 25.0,
+  },
+
+  {
+    title: 'carding g/GG',
+    description: `consiste em uma técnica de escovação indolor na qual é realizada a retirada de subpelos (pelos mortos). com ela, aqueles pelinhos mais finos e não vistos, que sempre caem pela casa ou formam nós, são eliminados de forma rápida e eficiente.
+`,
+    price: 50.0,
+  },
+
+  { title: 'corte de unha cães/gatos/coelhos', description: ``, price: 20.0 },
 ];
