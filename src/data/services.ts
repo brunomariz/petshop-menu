e9c28365e39584f04bbd0efe1a4b9c5e97cdc5f3
Bruno Mariz <bruno.mariz@semantix.inc>
2023-04-27 16:00:57 -0300

    feat: alterar precos

--- src/data/services.ts
+++ src/data/services.ts
@@ -1,100 +1,118 @@
-import { PetshopService } from '../@types/petshopService';
+import { PetshopService } from "../@types/petshopService";
 
 export const services: PetshopService[] = [
   {
-    title: 'banho: PP',
+    title: "banho: PP",
     description: `banho secagem e escovaçao da pelagem. incluso escovação de dentes, limpeza de orelhas, corte de unhas, perfume, enfeite
 
 animais até 3 kg e das seguintes raças:
 pincher/chihuahua PP, yorkshire PP`,
-    price: 55.0,
-  },
-
-  {
-    title: 'banho: p',
-    description: `banho secagem e escovaçao da pelagem. incluso escovação de dentes, limpeza de orelhas, corte de unhas, perfume, enfeite
-
-animais até 10 kg e das seguintes raças:
-bulldog francês, dachshund, fox, jack russel, spitz pequeno, maltês, pincher, poodle P, pug, galgo italiano, srd P, schnauzer, shih tzu pelo curto, lhasa pelo curto, pequinês, yorkshire p`,
     price: 60.0,
   },
 
   {
-    title: 'banho: m',
+    title: "banho: p",
+    description: `banho secagem e escovaçao da pelagem. incluso escovação de dentes, limpeza de orelhas, corte de unhas, perfume, enfeite
+
+animais até 10 kg e das seguintes raças:
+bulldog francês, dachshund, fox, jack russel, spitz pequeno, maltês, pincher, poodle P, pug, galgo italiano, srd P, schnauzer, shih tzu pelo curto, lhasa pelo curto, pequinês, yorkshire p`,
+    price: 65.0,
+  },
+
+  {
+    title: "banho: m",
     description: `banho secagem e escovaçao da pelagem. incluso escovação de dentes, limpeza de orelhas, corte de unhas, perfume, enfeite
 
 animais até 20 kg e das seguintes raças:
 beagle, bichon frisé, cocker, cavalier, coton, welsh corgi, srdm, spitz medio, spitz japones, vzsla, whippet, pastor de shetland, west higland terrier, animais tamanho P de pelagem longa`,
-    price: 70.0,
+    price: 75.0,
   },
 
   {
-    title: 'banho: g',
+    title: "banho: g",
     description: `banho secagem e escovaçao da pelagem. incluso escovação de dentes, limpeza de orelhas, corte de unhas, perfume, enfeite
 
 border collie, buldog ingles, bull terrier, boxer, basset hound, dalmata, pit bull, srd g, weimaraner`,
-    price: 80.0,
+    price: 85.0,
   },
 
   {
-    title: 'banho: GG',
+    title: "banho: GG",
     description: `banho secagem e escovaçao da pelagem. incluso escovação de dentes, limpeza de orelhas, corte de unhas, perfume, enfeite
 
 akita, golden, pastor alemão, samoieda, chow chow, husky siberiano, labrador, rhodesian, rottweiler`,
-    price: 110.0,
+    price: 120.0,
   },
 
   {
-    title: 'banho: xGG',
+    title: "banho: xGG",
     description: `banho secagem e escovaçao da pelagem. incluso escovação de dentes, limpeza de orelhas, corte de unhas, perfume, enfeite
 
 bernese, dogue alemão, kuvasz`,
-    price: 130.0,
+    price: 150.0,
   },
 
   {
-    title: 'banho: gatos',
+    title: "banho: gatos",
     description: `banho secagem e escovaçao da pelagem. incluso limpeza de orelhas, corte de unhas, perfume, enfeite
 
 srd, siames, persa, sagrado da birmania, `,
-    price: 90.0,
+    price: 95.0,
   },
 
   {
-    title: 'tosa higienica p',
-    description: `corte dos pelos das regiões: embaixo das patinhas , ao redor do  nus e barriga. realizada nos animais de tamanho P e m
+    title: "tosa higienica p",
+    description: `corte dos pelos das regiões: embaixo das patinhas , ao redor do  nus e barriga. realizada nos animais de tamanho P
 `,
-    price: 20.0,
+    price: 25.0,
   },
   {
-    title: 'tosa higienica g/GG',
+    title: "tosa higienica m",
+    description: `corte dos pelos das regiões: embaixo das patinhas , ao redor do  nus e barriga. realizada nos animais de tamanho m
+`,
+    price: 35.0,
+  },
+  {
+    title: "tosa higienica g/GG",
     description: `corte dos pelos das regiões: embaixo das patinhas , ao redor do  nus e barriga. realizada nos animais de tamanho g e GG
 `,
-    price: 40.0,
+    price: 45.0,
   },
 
   {
-    title: 'meia tosa p',
+    title: "meia tosa p",
     description: `corte dos pelos das regiões: embaixo das patinhas , ao redor do  nus e barriga e corte dos pelos da região da cabeça realizada nos animais de tamanho p
 `,
-    price: 30.0,
+    price: 35.0,
   },
 
   {
-    title: 'tosa ',
+    title: "tosa",
     description: `corte dos pelos de todo o corpo, deixando o pelo no modelo e altura solicitados
 `,
-    price: 90.0,
+    price: 95.0,
   },
 
   {
-    title: 'mascara de hidratação p/M - sense',
+    title: "mascara de hidratação p - sense",
     description: `complexo de lipoproteínas que hidrata e recupera a pelagem, contém extrato de aloe vera, que ajuda a hidratar, tonificar e condicionar os fios.`,
     price: 40.0,
   },
 
   {
-    title: 'mascara de nutrição p/M -  flash',
+    title: "mascara de hidratação M - sense",
+    description: `complexo de lipoproteínas que hidrata e recupera a pelagem, contém extrato de aloe vera, que ajuda a hidratar, tonificar e condicionar os fios.`,
+    price: 60.0,
+  },
+
+  {
+    title: "mascara de hidratação G - sense",
+    description: `complexo de lipoproteínas que hidrata e recupera a pelagem, contém extrato de aloe vera, que ajuda a hidratar, tonificar e condicionar os fios.`,
+    price: 80.0,
+  },
+
+  {
+    title: "mascara de nutrição p/M -  flash",
     description: `ação termo protetora: protege dos danos causados pelo calor do secador e chapinha;
 • contém óleo de argan, que hidrata, aumenta a elasticidade e diminui o frizz;
 • indicado para cães e gatos de todas as raças`,
@@ -102,7 +120,7 @@ srd, siames, persa, sagrado da birmania, `,
   },
 
   {
-    title: 'mascara de reconstrução p/M -  liso',
+    title: "mascara de reconstrução p/M -  liso",
     description: `máscara de hidratação multifuncional;
 • promove a reconstrução e recuperação da pelagem ;
 • proporciona brilho, melhor acabamento e maciez à pelagem, deixando-a mais lisa. • contém óleos de crambe e moringa, que têm ação condicionante e melhora a penteabilidade; além de queratina, creatina e d-pantenol, que têm ação reestruturadora, restauradora, hidratante e umectante;
@@ -111,7 +129,7 @@ srd, siames, persa, sagrado da birmania, `,
   },
 
   {
-    title: 'mascara de reconstrução p/M -  volume',
+    title: "mascara de reconstrução p/M -  volume",
     description: `máscara de hidratação multifuncional;
 • promove a reconstrução e recuperação da pelagem;
 • confere brilho e maciez;
@@ -121,25 +139,25 @@ srd, siames, persa, sagrado da birmania, `,
   },
 
   {
-    title: 'desembolo ',
+    title: "desembolo ",
     description: `escovaçao com uso de fluido desembaraçante que remove os nós que se formam nos animais de pelagem média ou longa
 `,
-    price: 'cobrado de acordo com avaliação individual',
+    price: "cobrado de acordo com avaliação individual",
   },
 
   {
-    title: 'carding p/M',
+    title: "carding p/M",
     description: `consiste em uma técnica de escovação indolor na qual é realizada a retirada de subpelos (pelos mortos). com ela, aqueles pelinhos mais finos e não vistos, que sempre caem pela casa ou formam nós, são eliminados de forma rápida e eficiente.
 `,
     price: 25.0,
   },
 
   {
-    title: 'carding g/GG',
+    title: "carding g/GG",
     description: `consiste em uma técnica de escovação indolor na qual é realizada a retirada de subpelos (pelos mortos). com ela, aqueles pelinhos mais finos e não vistos, que sempre caem pela casa ou formam nós, são eliminados de forma rápida e eficiente.
 `,
     price: 50.0,
   },
 
-  { title: 'corte de unha cães/gatos/coelhos', description: ``, price: 20.0 },
+  { title: "corte de unha cães/gatos/coelhos", description: ``, price: 20.0 },
 ];
