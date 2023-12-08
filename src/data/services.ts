1d6e88d77b0ee4207d829746913894c191fd1c71
brunomariz <48870924+brunomariz@users.noreply.github.com>
2023-12-08 17:54:10 -0300

    Update services.ts

--- src/data/services.ts
+++ src/data/services.ts
@@ -7,7 +7,7 @@ export const services: PetshopService[] = [
 
 animais até 3 kg e das seguintes raças:
 pincher/chihuahua PP, yorkshire PP`,
-    price: 60.0,
+    price: 65.0,
   },
 
   {
@@ -16,7 +16,7 @@ pincher/chihuahua PP, yorkshire PP`,
 
 animais até 10 kg e das seguintes raças:
 bulldog francês, dachshund, fox, jack russel, spitz pequeno, maltês, pincher, poodle P, pug, galgo italiano, srd P, schnauzer, shih tzu pelo curto, lhasa pelo curto, pequinês, yorkshire p`,
-    price: 65.0,
+    price: 70.0,
   },
 
   {
@@ -25,7 +25,7 @@ bulldog francês, dachshund, fox, jack russel, spitz pequeno, maltês, pincher, 
 
 animais até 20 kg e das seguintes raças:
 beagle, bichon frisé, cocker, cavalier, coton, welsh corgi, srdm, spitz medio, spitz japones, vzsla, whippet, pastor de shetland, west higland terrier, animais tamanho P de pelagem longa`,
-    price: 75.0,
+    price: 80.0,
   },
 
   {
@@ -33,7 +33,7 @@ beagle, bichon frisé, cocker, cavalier, coton, welsh corgi, srdm, spitz medio, 
     description: `banho secagem e escovaçao da pelagem. incluso escovação de dentes, limpeza de orelhas, corte de unhas, perfume, enfeite
 
 border collie, buldog ingles, bull terrier, boxer, basset hound, dalmata, pit bull, srd g, weimaraner`,
-    price: 85.0,
+    price: 90.0,
   },
 
   {
@@ -41,7 +41,7 @@ border collie, buldog ingles, bull terrier, boxer, basset hound, dalmata, pit bu
     description: `banho secagem e escovaçao da pelagem. incluso escovação de dentes, limpeza de orelhas, corte de unhas, perfume, enfeite
 
 akita, golden, pastor alemão, samoieda, chow chow, husky siberiano, labrador, rhodesian, rottweiler`,
-    price: 115.0,
+    price: 120.0,
   },
 
   {
@@ -49,7 +49,7 @@ akita, golden, pastor alemão, samoieda, chow chow, husky siberiano, labrador, r
     description: `banho secagem e escovaçao da pelagem. incluso escovação de dentes, limpeza de orelhas, corte de unhas, perfume, enfeite
 
 bernese, dogue alemão, kuvasz`,
-    price: 150.0,
+    price: 155.0,
   },
 
   {
@@ -57,58 +57,58 @@ bernese, dogue alemão, kuvasz`,
     description: `banho secagem e escovaçao da pelagem. incluso limpeza de orelhas, corte de unhas, perfume, enfeite
 
 srd, siames, persa, sagrado da birmania, `,
-    price: 90.0,
+    price: 95.0,
   },
 
   {
     title: "tosa higienica p",
     description: `corte dos pelos das regiões: embaixo das patinhas , ao redor do  nus e barriga. realizada nos animais de tamanho P
 `,
-    price: 25.0,
+    price: 30.0,
   },
   {
     title: "tosa higienica m",
     description: `corte dos pelos das regiões: embaixo das patinhas , ao redor do  nus e barriga. realizada nos animais de tamanho m
 `,
-    price: 35.0,
+    price: 40.0,
   },
   {
     title: "tosa higienica g/GG",
     description: `corte dos pelos das regiões: embaixo das patinhas , ao redor do  nus e barriga. realizada nos animais de tamanho g e GG
 `,
-    price: 45.0,
+    price: 50.0,
   },
 
   {
     title: "meia tosa p",
     description: `corte dos pelos das regiões: embaixo das patinhas , ao redor do  nus e barriga e corte dos pelos da região da cabeça realizada nos animais de tamanho p
 `,
-    price: 35.0,
+    price: 40.0,
   },
 
   {
     title: "tosa",
     description: `corte dos pelos de todo o corpo, deixando o pelo no modelo e altura solicitados
 `,
-    price: 95.0,
+    price: 100.0,
   },
 
   {
     title: "mascara de hidratação p - sense",
     description: `complexo de lipoproteínas que hidrata e recupera a pelagem, contém extrato de aloe vera, que ajuda a hidratar, tonificar e condicionar os fios.`,
-    price: 40.0,
+    price: 45.0,
   },
 
   {
     title: "mascara de hidratação M - sense",
     description: `complexo de lipoproteínas que hidrata e recupera a pelagem, contém extrato de aloe vera, que ajuda a hidratar, tonificar e condicionar os fios.`,
-    price: 60.0,
+    price: 65.0,
   },
 
   {
     title: "mascara de hidratação G - sense",
     description: `complexo de lipoproteínas que hidrata e recupera a pelagem, contém extrato de aloe vera, que ajuda a hidratar, tonificar e condicionar os fios.`,
-    price: 80.0,
+    price: 85.0,
   },
 
   {
@@ -116,7 +116,7 @@ srd, siames, persa, sagrado da birmania, `,
     description: `ação termo protetora: protege dos danos causados pelo calor do secador e chapinha;
 • contém óleo de argan, que hidrata, aumenta a elasticidade e diminui o frizz;
 • indicado para cães e gatos de todas as raças`,
-    price: 20.0,
+    price: 25.0,
   },
 
   {
@@ -125,7 +125,7 @@ srd, siames, persa, sagrado da birmania, `,
 • promove a reconstrução e recuperação da pelagem ;
 • proporciona brilho, melhor acabamento e maciez à pelagem, deixando-a mais lisa. • contém óleos de crambe e moringa, que têm ação condicionante e melhora a penteabilidade; além de queratina, creatina e d-pantenol, que têm ação reestruturadora, restauradora, hidratante e umectante;
 • indicado para cães de pelagem danificada longa e lisa.`,
-    price: 20.0,
+    price: 25.0,
   },
 
   {
@@ -135,7 +135,7 @@ srd, siames, persa, sagrado da birmania, `,
 • confere brilho e maciez;
 • contém proteína do trigo e d-pantenol, que confere maior volume a pelagem, ação hidratante e umectante; além de queratina e creatina, que tem ação reestruturadora, restauradora e hidratante;
 • indicado para cães e gatos de pelagem danificada e volumosa;`,
-    price: 20.0,
+    price: 25.0,
   },
 
   {
@@ -149,22 +149,22 @@ srd, siames, persa, sagrado da birmania, `,
     title: "carding p",
     description: `consiste em uma técnica de escovação indolor na qual é realizada a retirada de subpelos (pelos mortos). com ela, aqueles pelinhos mais finos e não vistos, que sempre caem pela casa ou formam nós, são eliminados de forma rápida e eficiente.
 `,
-    price: 25.0,
+    price: 30.0,
   },
 
   {
     title: "carding M",
     description: `consiste em uma técnica de escovação indolor na qual é realizada a retirada de subpelos (pelos mortos). com ela, aqueles pelinhos mais finos e não vistos, que sempre caem pela casa ou formam nós, são eliminados de forma rápida e eficiente.
 `,
-    price: 30.0,
+    price: 35.0,
   },
 
   {
     title: "carding g/GG",
     description: `consiste em uma técnica de escovação indolor na qual é realizada a retirada de subpelos (pelos mortos). com ela, aqueles pelinhos mais finos e não vistos, que sempre caem pela casa ou formam nós, são eliminados de forma rápida e eficiente.
 `,
-    price: 50.0,
+    price: 55.0,
   },
 
-  { title: "corte de unha cães/gatos/coelhos", description: ``, price: 20.0 },
+  { title: "corte de unha cães/gatos/coelhos", description: ``, price: 25.0 },
 ];
